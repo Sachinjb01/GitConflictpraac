a372d411c7af9e1da3988a749715dda894424c7a
Sachinjb01 <sachinjb96@gmail.com>
2022-01-31 22:57:23 +0530

    dfdfd

--- src/user2/USer2Javaclass.java
+++ src/user2/USer2Javaclass.java
@@ -4,7 +4,7 @@ public class USer2Javaclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("USer 2 first comment");
+		System.out.println("USer 2 first commes");
 
 	}
 
